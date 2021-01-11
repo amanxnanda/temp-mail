ffc7110f5cd11859c440969cc8c89efab329aa90
Aman Nanda <34944818+amannanda8@users.noreply.github.com>
2021-01-11 19:54:18 +0530

    added image and font assets

---
+++ lib/constant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kYellowColor = Color.fromARGB(255, 254, 206, 47);
+const kWhiteColor = Color.fromARGB(255, 255, 255, 255);
+BoxDecoration kBoxDecoration = BoxDecoration(
+  color: Colors.transparent,
+  borderRadius: BorderRadius.circular(10.0),
+  border: Border.all(
+    width: 0.6,
+    color: Colors.black,
+  ),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:bordered_text/bordered_text.dart';
+import 'constant.dart';
 
 void main() => runApp(MyApp());
 
@@ -33,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 254, 206, 47),
+        backgroundColor: kYellowColor,
         title: Row(
           children: [
             Text(
@@ -48,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'MAIL',
               style: TextStyle(
                 letterSpacing: _width * 0.02,
-                color: Color.fromARGB(255, 255, 255, 255),
+                color: kWhiteColor,
                 fontWeight: FontWeight.w900,
               ),
             )
@@ -56,19 +59,107 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: Container(
-        color: Color.fromARGB(255, 255, 255, 255),
+        color: kWhiteColor,
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Image.asset(
                 'Assets/hitting_mail.png',
                 filterQuality: FilterQuality.high,
                 fit: BoxFit.scaleDown,
               ),
+              Container(
+                decoration: kBoxDecoration.copyWith(
+                    borderRadius: BorderRadius.circular(_width * 0.03)),
+                margin: EdgeInsets.all(_width * 0.05),
+                height: _height * 0.09,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8, 8, 15, 8),
+                      child: Icon(
+                        Icons.mail,
+                        color: kYellowColor,
+                      ),
+                      
+                    ),
+                    BorderedText(
+                      strokeWidth: 1,
+                      child: Text(
+                        'amannanda8@gmail.com',
+                        style: TextStyle(
+                          color: kYellowColor,
+                          fontSize: _width * 0.07,
+                          fontWeight: FontWeight.w600,
+                          //fontFamily: 'LuckiestGuy',
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CopyAndRefresh(
+                      width: _width,
+                      height: _height,
+                      text: 'Regenerate',
+                      icon: Icons.refresh),
+                  CopyAndRefresh(
+                    width: _width,
+                    height: _height,
+                    text: 'Copy email',
+                    icon: Icons.copy,
+                  ),
+                ],
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class CopyAndRefresh extends StatelessWidget {
+  const CopyAndRefresh({
+    Key key,
+    @required double width,
+    @required double height,
+    @required this.text,
+    @required this.icon,
+  })  : _width = width,
+        _height = height,
+        super(key: key);
+
+  final double _width;
+  final double _height;
+  final String text;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: _width * 0.3,
+      height: _height * 0.07,
+      margin: EdgeInsets.symmetric(horizontal: _width * 0.05),
+      decoration: kBoxDecoration.copyWith(),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(2.0),
+            child: Icon(icon),
+          ),
+          Text(
+            text,
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
