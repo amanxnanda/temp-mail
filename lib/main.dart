260caad74036f1748dd6323fe6da22a635129ee1
Aman Nanda <34944818+amannanda8@users.noreply.github.com>
2021-01-11 14:08:06 +0530

    added radial gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,89 +7,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        
-        primarySwatch: Colors.red,
-        
+        primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(8.0),
-              child: LottieBuilder.asset(
-                'Assets/mail2.json',
-                frameRate: FrameRate(60),
-                fit: BoxFit.cover,
-              ),
-            ),
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                FloatingActionButton(
-                  onPressed: _decrementCounter,
-                  tooltip: 'Decrement',
-                  child: Icon(
-                    Icons.remove,
-                  ),
-                ),
-                FloatingActionButton(
-                  onPressed: _incrementCounter,
-                  tooltip: 'Increment',
-                  isExtended: true,
-                  child: Icon(
-                    Icons.add,
-                  ),
-                ),
-              ],
-            )
-          ],
+      appBar: AppBar(),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            
+            radius: 1.5,
+            colors: [
+              Color.fromARGB(255, 22, 135, 237),
+              
+              Color.fromARGB(255, 20, 55, 90)
+            ],
+          ),
         ),
       ),
     );
